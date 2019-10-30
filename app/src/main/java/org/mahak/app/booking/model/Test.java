7c462460bd02653871598d41fd3eb9b7ddb63f5b
mimdal92 <maryam.dongi@yahoo.com>
2019-10-31 01:21:16 +0330

    test class relaion

---
+++ app/src/main/java/org/mahak/app/booking/model/Test.java
@@ -0,0 +1,17 @@
+package org.mahak.app.booking.model;
+
+public class Test {
+    Shape.MyClass2 c=new Shape.MyClass2();
+    void f()
+    {
+        Shape a=new Shape();
+//        Shape.MyClass b=a.new MyClass();
+        try {
+            a.f();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
--- app/src/main/java/org/mahak/app/booking/ui/MainActivity.java
+++ app/src/main/java/org/mahak/app/booking/ui/MainActivity.java
@@ -3,6 +3,7 @@ package org.mahak.app.booking.ui;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.DialogInterface;
 import android.os.Bundle;
 
@@ -17,8 +18,28 @@ public class MainActivity extends BookingActivity {
         setContentView(R.layout.activity_main);
 
     }
-    private void f()
+
+    private void f() {
+
+    }
+
+    class Dialog {
+        void f() {
+            g(Dialog.this);
+            h(MainActivity.this);
+
+        }
+    }
+
+
+    void g(Dialog g)
     {
 
     }
+
+    void h(Activity a)
+    {
+
+    }
+
 }
